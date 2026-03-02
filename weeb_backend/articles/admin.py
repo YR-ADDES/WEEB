333f7332e62d2115596a6f25cf5c29814d0c0705
ADDES YASMINE <addes.dev@gmail.com>
2026-03-02 15:40:19 +0100

    [BACK]: ajout admin articles

--- weeb_backend/articles/admin.py
+++ weeb_backend/articles/admin.py
@@ -1,3 +1,19 @@
+## IMPORTS ##
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+## ADMIN ARTICLE ##
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    ## LISTE ##
+    list_display = ("id", "titre", "auteur", "date_creation")
+
+    ## RECHERCHE ##
+    search_fields = ("titre", "contenu", "auteur__email")
+
+    ## FILTRES ##
+    list_filter = ("date_creation",)
+
+    ## TRI ##
+    ordering = ("-date_creation",)
